34e1e7501c40a2d0945b96c65966059f9d27d8ca
Bali Tok <bali.tok@hotmail.com>
2023-03-07 15:09:35 +0800

    Added a new route to create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Article.create!(article_params)
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
