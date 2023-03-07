525748ff525d2c387155d34ebbaa101921da6f97
Bali Tok <bali.tok@hotmail.com>
2023-03-07 15:46:46 +0800

    refactored controller using before_action filter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article
   end
 
   def new
@@ -17,22 +19,24 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    @article
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to articles_path
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path, status: :see_other
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
